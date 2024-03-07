9edb51cc81845a1d7069ae10e79225b175b8d034
GaryRoberts <garyroberts1995@gmail.com>
2024-03-07 00:50:26 -0500

    changed project

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,25 @@
 <?php
 
-use App\Http\Controllers\ApiTokenController;
-use App\Http\Controllers\RepositoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+  
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+  
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+     
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
 });
-
-Route::post('/auth/login', [ApiTokenController::class, 'login']);
-Route::post('/auth/register', [ApiTokenController::class, 'register']);
-Route::middleware('auth:sanctum')->post('/auth/logout', [ApiTokenController::class, 'logout']);
-
-Route::middleware('auth:sanctum')->get('/repo', [RepositoryController::class, 'index']);
-//Route::middleware(['auth:sanctum', 'abilities:repo-view'])->get('/repo', [RepositoryController::class, 'index']);
-
-Route::middleware('auth:sanctum')->post('/repo', [RepositoryController::class, 'store']);
-//Route::middleware(['auth:sanctum', 'abilities:repo-create'])->post('/repo', [RepositoryController::class, 'index']);
